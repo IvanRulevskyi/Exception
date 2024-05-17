e4382b26745a89829802202fe7caa1091eda65d0
IvanRulevskyi <rulevskiy4@gmail.com>
2024-05-17 19:36:44 +0300

    Initial Commit

---
+++ Exception/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exception
+//
+//  Created by Иван Рулевский on 17.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
